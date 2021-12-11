4a7bcbeefcb03f101a77fb7eb541d8254268ef1d
Talha-Altair <cooltalha27@gmail.com>
2021-12-11 17:32:59 +0530

    Updates for 2021-12-11 17:32:59

---
+++ app/storage_handler.py
@@ -0,0 +1,35 @@
+from google.cloud import storage
+
+storage_client = storage.Client()
+
+BUCKET_NAME = "altair-janga"
+
+bucket = storage_client.get_bucket(BUCKET_NAME)
+
+def list_contents():
+
+    blobs = list(bucket.list_blobs())
+
+    for blob in blobs:
+
+        print(blob.name)
+
+    return blobs
+
+def read_file(file_name):
+
+    blob = bucket.blob(file_name)
+
+    return blob.download_as_string()
+
+def upload_file(file_name, file_data):
+
+    blob = bucket.blob(file_name)
+
+    blob.upload_from_string(file_data)
+
+    return blob
+
+if __name__ == "__main__":
+
+    print(list_contents())
