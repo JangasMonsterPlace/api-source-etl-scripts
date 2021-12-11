3eb158fe400afd61dfbcbd8b1b3b6c98ce2a9b3b
Talha-Altair <cooltalha27@gmail.com>
2021-12-11 17:43:08 +0530

    Updates for 2021-12-11 17:43:08

--- app/storage_handler.py
+++ app/storage_handler.py
@@ -1,6 +1,8 @@
 from google.cloud import storage
 from dotenv import load_dotenv
 
+import json
+
 load_dotenv()
 
 storage_client = storage.Client()
@@ -46,6 +48,16 @@ def delete_bucket(bucket_name):
     bucket.delete()
 
     return bucket
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    return blob
 
 if __name__ == "__main__":
 
