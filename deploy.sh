4fa9de87fde955ab4d6ad8facd82c8cd83ace3f3
JangasCodingplace <janisgoesser92@gmail.com>
2021-12-12 15:24:15 +0100

    Opt build.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
-docker stop api-source-etl-scripts
+docker stop api-source-etl-scripts-twitter
+docker stop api-source-etl-scripts-csv
+docker stop logstash-service-etl-twitter
+docker stop logstash-service-etl-csv
 
 git pull origin master
 
-docker-compose up --build -d api-source-etl-scripts
+mode=twitter docker-compose up --build -d api-source-etl-scripts
+mode=csv docker-compose up --build -d api-source-etl-scripts
